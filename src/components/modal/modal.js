835e8d2cb49e4677d7c718b01cbbd21b1217eb0e
AdelinaLipsa <adelina.lipsa@gmail.com>
2022-10-13 20:58:13 +0300

    design update

--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -63,44 +63,47 @@ const Modal = (props) => {
 
   return (
     <section className={"modal"}>
-      <div className={"btn-wrapper"}>
-        <button className={"btn-close"} onClick={handleModal}>⨉</button>
-      </div>
       <div>
-        <h3>Incearcă produsul selectând una din culorile de mai jos</h3>
-        <br/>
-        <div className={"canvas-wrapper"}>
-          <canvas className="deepar"
-                  id="deepar-canvas"
-                  onContextMenu={(e) => handleMouseEvent(e)}
-                  width={fullScreen ? window.innerWidth + 'px' : '900px'}
-                  height={fullScreen ? window.innerHeight + 'px' : '600px'}
-          ></canvas>
-          <button className={"fullscreen"} onClick={() => fullScreen ? setFullScreen(false) : setFullScreen(true)}>
-            <svg height="50" viewBox="0 0 50 50" width="50" xmlns="http://www.w3.org/2000/svg">
-              <path d="M0 0h48v48h-48z" fill="none"/>
-              <path
-                d="M14 28h-4v10h10v-4h-6v-6zm-4-8h4v-6h6v-4h-10v10zm24 14h-6v4h10v-10h-4v6zm-6-24v4h6v6h4v-10h-10z"/>
-            </svg>
-          </button>
-        </div>
-        <div className={"colors-wrapper"}>
-          {colors.map((color, index) => {
-            return <div key={index} className={"colors"}>
-              <label
-                className="radio-button-label">
-                <input type="radio" name="color-choice"
-                       value={JSON.stringify(color.filterData[0]['Filter Binary Path'])} className="sr-only"
-                       onChange={handleFilterClick}/>
-                <div style={{
-                  backgroundColor: color.filterData[0]['Hex Color'],
-                  width: '50px',
-                  height: '50px',
-                  cursor: "pointer"
-                }}/>
-              </label>
+        <div className={"modal-content-wrapper"}>
+          <div className={"canvas-wrapper"}>
+            <div className={"btn-wrapper"}>
+              <button className={"btn-close"} onClick={handleModal}>⨉</button>
             </div>
-          })}
+            <canvas className="deepar"
+                    id="deepar-canvas"
+                    onContextMenu={(e) => handleMouseEvent(e)}
+                    width={fullScreen ? window.innerWidth + 'px' : '900px'}
+                    height={fullScreen ? window.innerHeight + 'px' : '600px'}
+            ></canvas>
+          </div>
+          <div className={"buttons"}>
+            <div className={"colors-wrapper"}>
+              {colors.map((color, index) => {
+                return <div key={index} className={"colors"}>
+                  <label
+                    className="radio-button-label">
+                    <input type="radio" name="color-choice"
+                           value={JSON.stringify(color.filterData[0]['Filter Binary Path'])} className="sr-only"
+                           onChange={handleFilterClick}/>
+                    <div style={{
+                      backgroundColor: color.filterData[0]['Hex Color'],
+                      width: '50px',
+                      height: '50px',
+                      cursor: "pointer",
+                      borderRadius: "100%"
+                    }}/>
+                  </label>
+                </div>
+              })}
+            </div>
+            <button className={"fullscreen"} onClick={() => fullScreen ? setFullScreen(false) : setFullScreen(true)}>
+              <svg height="50" viewBox="0 0 50 50" width="50" xmlns="http://www.w3.org/2000/svg">
+                <path d="M0 0h48v48h-48z" fill="none"/>
+                <path
+                  d="M14 28h-4v10h10v-4h-6v-6zm-4-8h4v-6h6v-4h-10v10zm24 14h-6v4h10v-10h-4v6zm-6-24v4h6v6h4v-10h-10z"/>
+              </svg>
+            </button>
+          </div>
         </div>
       </div>
     </section>
