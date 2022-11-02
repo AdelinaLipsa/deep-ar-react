be41233c7bc4788682f553889b37a38b728dd430
AdelinaLipsa <adelina.lipsa@gmail.com>
2022-11-02 19:13:50 +0200

    wip

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
+const cors = require("cors");
 let app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 80;
+const corsOptions = {
+  origin: 'https://farmec.herokuapp.com',
+  optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions));
+app.options('*', cors());
+
 app.listen(port);
