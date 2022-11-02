9141b5023097bcc72f71d2e1a8b88231eb2b5a46
AdelinaLipsa <adelina.lipsa@gmail.com>
2022-11-02 17:14:00 +0200

    changed server

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-const express = require('express');
-const serveStatic = require("serve-static")
-const path = require('path');
-let app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
-const port = process.env.PORT;
-app.listen(port || 5000);
+const express = require('express'); //Line 1
+const app = express(); //Line 2
+const port = process.env.PORT || 5000; //Line 3
+
+// This displays message that the server running and listening to specified port
+app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
