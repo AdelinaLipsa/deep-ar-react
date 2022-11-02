8fae91ca3947f8971a9d765e64a9f64ceed2e62d
AdelinaLipsa <adelina.lipsa@gmail.com>
2022-11-02 18:07:58 +0200

    wip

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
-const express = require('express');
-const serveStatic = require("serve-static")
-const path = require('path');
-const cors = require("cors");
-let app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
-const port = process.env.PORT || 80;
-app.use(cors());
-app.listen(port);
+const express = require("express")
+const cors = require("cors")
+const app = express()
+const port = process.env.PORT || 3000
+
+const whitelist = ["http://localhost:3000"]
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error("Not allowed by CORS"))
+    }
+  },
+  credentials: true,
+}
+app.use(cors(corsOptions))
+
+app.get("/", (req, res) => {
+  res.send({ message: "Hello World!" })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at Port: ${port}`)
+})
