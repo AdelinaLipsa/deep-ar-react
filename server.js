85c34a71f5e57503c96a36ccfadd8c578319b26b
AdelinaLipsa <adelina.lipsa@gmail.com>
2022-11-02 16:51:13 +0200

    wip

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const serveStatic = require("serve-static")
 const path = require('path');
 let app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
-const port = process.env.PORT || 80;
-app.listen(port);
+const port = process.env.PORT;
+app.listen(port || 5000);
